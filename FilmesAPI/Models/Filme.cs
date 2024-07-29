8abf34d249ea2bfd9adf054c47a7b3ec2dd698fa
Davi Roberto <daviroberto11@hotmail.com>
2024-07-29 13:53:07 -0300

    Validando parâmetros recebidos

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FilmesAPI.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O título do filme é obrigatório.")]
     public string Titulo { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O gênero é obrigatório.")]
+    [MaxLength(ErrorMessage = "O tamanho do gênero não pode exceder 50 casracteres.")]
     public string Genero { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "A duração é obrigatória.")]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
     public int Duracao { get; set; }
 
 }
