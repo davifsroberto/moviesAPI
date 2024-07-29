ffd5806f4a0b4c66d3d8abbd9598e73385400746
Davi Roberto <daviroberto11@hotmail.com>
2024-07-29 17:08:08 -0300

    Conectando ao banco de dados

--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,15 +1,17 @@
+using FilmesAPI;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("FilmeConnection");
 
-// Add services to the container.
-
+builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
