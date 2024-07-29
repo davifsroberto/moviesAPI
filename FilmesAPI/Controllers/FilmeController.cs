01848038a751733033acedc707c94374583f249b
Davi Roberto <daviroberto11@hotmail.com>
2024-07-29 15:49:01 -0300

    Paginando resultados

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -9,10 +9,24 @@ namespace Name.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionarFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperarFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,6 +4,8 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
+
     [Required(ErrorMessage = "O título do filme é obrigatório.")]
     public string Titulo { get; set; } = string.Empty;
 
@@ -14,5 +16,4 @@ public class Filme
     [Required(ErrorMessage = "A duração é obrigatória.")]
     [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos")]
     public int Duracao { get; set; }
-
 }
